6396dd3a6afa642d22a227b26f8d63272f7e5873
Samantha Arellano <Sambollano54@gmail.com>
2021-06-20 12:29:55 -0500

    .

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(require('./'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
